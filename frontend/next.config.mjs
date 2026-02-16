4aee9330f44db16fec1af58c04c6266e843afe6c
Fuad2004 <fuadalfiroj2004@gmail.com>
2026-02-17 00:14:39 +0600

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,13 +2,7 @@
 const nextConfig = {
   reactCompiler: true,
   serverExternalPackages: ['sequelize', 'pg', 'pg-hstore'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore pg-native completely
-      config.externals.push('pg-native');
-    }
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
